1eee47c1431c9d42aa3be36d738b8dd015a7a98c
toncho11 <toncho11@gmail.com>
2019-11-19 14:17:20 +0100

    updated doc

--- src/svm.jl
+++ src/svm.jl
@@ -20,7 +20,7 @@ using PosDefManifoldML
 # generate data
 PTr, PTe, yTr, yTe=gen2ClassData(10, 30, 40, 60, 80)
 
-# create and train an SVM model
+# create and train an SVM model with default SVM parameters
 model=fit(svm(), PTr, yTr)
 
 # predict using this model
@@ -47,12 +47,59 @@ mutable struct svm <: TSmodel
                            		  kernel = Kernel.RadialBasis,
 								 epsilon = 0.1,
 								    cost = 1.0,
-								   gamma = -1
+								   gamma = -1 #can not set a default value herem because it requires the number of observations
 								 )
 	   	 			 new(metric, internalModel, meanISR,
-                         svmtype, kernel, epsilon, cost, gamma) #can not set a default value here
+                         svmtype, kernel, epsilon, cost, gamma)
     end
 end
+
+"""
+```
+mutable struct svm <: TSmodel
+    	metric        :: Metric
+		internalModel
+		meanISR
+
+		svmtype       ::Type
+		kernel        ::Kernel.KERNEL
+		epsilon       ::Float64
+		cost          ::Float64
+		gamma         ::Float64
+end
+```
+
+`.metric`, of type
+[Metric](https://marco-congedo.github.io/PosDefManifold.jl/dev/MainModule/#Metric::Enumerated-type-1),
+is the metric that will be adopted to compute the mean used
+as base-point for tangent space projection. By default the
+Fisher metric is adopted. See [mdm.jl](@ref)
+for the available metrics. If the data used to train the model
+are not positive definite matrices, but Euclidean feature vectors,
+the `.metric` field has no use.
+
+`.meanISR` is optionally passed to the [`fit`](@ref)
+function. By default it is computed thereby.
+If the data used to train the model
+are not positive definite matrices, but Euclidean feature vectors,
+the `.meanISR` field has no use and is set to `nothing`.
+
+The following are parameters that are passed to the LIBSVM package:
+
+`svmtype::Type=LIBSVM.SVC`: Type of SVM to train `SVC` (for C-SVM), `NuSVC`
+    `OneClassSVM`, `EpsilonSVR` or `NuSVR`. Defaults to `OneClassSVM` if
+    `y` is not used
+
+`kernel::Kernels.KERNEL=Kernel.RadialBasis`: Model kernel `Linear`, `polynomial`,
+    `RadialBasis`, `Sigmoid` or `Precomputed`
+
+`gamma::Float64=1.0/size(X, 1)` : γ for kernels
+
+`cost::Float64=1.0`: cost parameter C of C-SVC, epsilon-SVR, and nu-SVR
+
+`epsilon::Float64=0.1`: epsilon in loss function of epsilon-SVR
+"""
+
 
 function fit(model :: svm,
                𝐏Tr :: Union{ℍVector, Matrix{Float64}},
